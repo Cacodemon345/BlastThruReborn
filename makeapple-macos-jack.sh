414bc9d7888ba8d16b1748824f1c06151442ff28
sahil <sahil@iMac.local>
2020-07-30 12:30:54 +0600

    Revert required C++ standard version to C++17 for POSIX build scripts

--- makeapple-macos-jack.sh
+++ makeapple-macos-jack.sh
@@ -1,2 +1,2 @@
 # Alternate macOS compile script in case Apple drops CoreMIDI support in their OS. May or may not work.
-clang++ -std=gnu++2a MidsFileParser.cpp BTRPlayArea.cpp ConsoleApplication9.cpp  SoundPlayback.cpp -lsfml-graphics -lsfml-system -lsfml-window -lstdc++ -lm -lpthread -lsndfile -lopenal -g -I ./  -I ./RtMidi17/ -L/usr/local/opt/openal-soft/lib -I/usr/local/opt/openal-soft/include -DRTMIDI17_JACK
+clang++ -std=gnu++17 MidsFileParser.cpp BTRPlayArea.cpp ConsoleApplication9.cpp  SoundPlayback.cpp -lsfml-graphics -lsfml-system -lsfml-window -lstdc++ -lm -lpthread -lsndfile -lopenal -g -I ./  -I ./RtMidi17/ -L/usr/local/opt/openal-soft/lib -I/usr/local/opt/openal-soft/include -DRTMIDI17_JACK
--- makeapple-macos.sh
+++ makeapple-macos.sh
@@ -1,3 +1,3 @@
 # Having to deal with the macOS's ass development tools is annoying as fuck.
 # Wish someone else had the balls to maintain macOS support for the game engine...
-clang++ -std=gnu++2a MidsFileParser.cpp BTRPlayArea.cpp ConsoleApplication9.cpp  SoundPlayback.cpp -lsfml-graphics -lsfml-system -lsfml-window -lstdc++ -lm -lpthread -lsndfile -lopenal -framework CoreMIDI -framework CoreFoundation -framework CoreAudio -g -I ./  -I ./RtMidi17/ -L/usr/local/opt/openal-soft/lib -I/usr/local/opt/openal-soft/include -DRTMIDI17_COREAUDIO
+clang++ -std=gnu++17 MidsFileParser.cpp BTRPlayArea.cpp ConsoleApplication9.cpp  SoundPlayback.cpp -lsfml-graphics -lsfml-system -lsfml-window -lstdc++ -lm -lpthread -lsndfile -lopenal -framework CoreMIDI -framework CoreFoundation -framework CoreAudio -g -I ./  -I ./RtMidi17/ -L/usr/local/opt/openal-soft/lib -I/usr/local/opt/openal-soft/include -DRTMIDI17_COREAUDIO
