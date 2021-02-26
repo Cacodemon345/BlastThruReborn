9d9b5df908c0818edf3471a63e036bb91d6ddd5c
Cacodemon345 <wahil1976@outlook.com>
2021-02-26 20:32:31 +0600

    Update build scripts

--- makelinux-clang-fluidsynth.sh
+++ makelinux-clang-fluidsynth.sh
@@ -1,1 +1,1 @@
-clang++ -std=gnu++17 FlSynthMidsFileParser.cpp BTRPlayArea.cpp ConsoleApplication9.cpp  SoundPlayback.cpp -lgcc -lsfml-graphics -lsfml-system -lsfml-window -lstdc++ -lm -lpthread -lasound -lopenal -lGL -lsndfile -lfluidsynth -lrt -g -I ./  -I ./RtMidi17/ -DRTMIDI17_ALSA
+clang++ -std=gnu++17 FlSynthMidsFileParser.cpp BTRPlayArea.cpp ConsoleApplication9.cpp  SoundPlayback.cpp -lgcc -lsfml-graphics -lsfml-system -lsfml-window -lstdc++ -lm -lpthread -lasound -lopenal -lGL -lsndfile -lfluidsynth -lrt -g -I ./
--- makelinux-clang.sh
+++ makelinux-clang.sh
@@ -1,1 +1,1 @@
-clang++ -std=gnu++17 MidsFileParser.cpp BTRPlayArea.cpp ConsoleApplication9.cpp  SoundPlayback.cpp -lgcc -lsfml-graphics -lsfml-system -lsfml-window -lstdc++ -lm -lpthread -lasound -lopenal -lGL -lsndfile -lrt -g -I ./  -I ./RtMidi17/ -DRTMIDI17_ALSA
+clang++ -std=gnu++17 MidsFileParser.cpp BTRPlayArea.cpp ConsoleApplication9.cpp  SoundPlayback.cpp -lgcc -lsfml-graphics -lsfml-system -lsfml-window -lstdc++ -lm -lpthread -lasound -lopenal -lGL -lsndfile -lrt -g -I ./  -I ./RtMidi17/  -I ./RtMidi17/include/ -DRTMIDI17_ALSA
